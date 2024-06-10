d15244a8fbea6b2772326eb8be7986a9c854a0c6
Ganesh <81965357+DittakaviHariNagaSuryaSaiGanesh@users.noreply.github.com>
2024-06-10 14:15:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,23 @@ const PORT = process.env.PORT;
 const ajantaApis = require('./routes/ajanta/userRouts');
 const userApi = require( './routes/login/loginRout' );
 const manipalApis = require( './routes/manipal/manipalRout' )
-const crosOPT = {
-    origin: "https://staging.multipliersolutions.com/",
-    originSuccessStatus: 200
-}
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://staging.multipliersolutions.com'
+];
+
+// Configure CORS options
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
 app.use( express.json() )
-app.use(cros(crosOPT))
+app.use(cros(corsOptions))
 
 app.use((req, res, next) => {
     next();
