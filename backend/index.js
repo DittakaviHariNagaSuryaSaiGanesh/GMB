0c1f98cf51dc42abef5a8985c5bc5b42dd99e90d
Ganesh <81965357+DittakaviHariNagaSuryaSaiGanesh@users.noreply.github.com>
2024-06-10 10:56:34 +0530

    build

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const cros = require('cors')
 const PORT = process.env.PORT;
 const ajantaApis = require('./routes/ajanta/userRouts');
 const userApi = require( './routes/login/loginRout' );
-const manipalApis = require('./routes/manipal/manipalRout')
-
+const manipalApis = require( './routes/manipal/manipalRout' )
+const crosOPT = {
+    origin: "localhost:3000",
+    originSuccessStatus: 200
+}
 app.use(express.json())
 
 app.use((req, res, next) => {
